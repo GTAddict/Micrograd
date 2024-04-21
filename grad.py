38f2ad141206a8cad1c3b95100c2ff03a959212b
GTAddict <soregtaddict@gmail.com>
2024-04-21 18:47:22 +0400

    Add visualization

--- grad.py
+++ grad.py
@@ -1,9 +1,32 @@
 import math
 import numpy as np
+from graphviz import Digraph
+
+def graph(value):
+        open = set()
+        open.add(value)
+        visited = set()
+        graph = Digraph(format='svg', graph_attr={'rankdir' : 'LR'})
+
+        while len(open):
+            node = open.pop()
+            if node not in visited:
+                visited.add(node)
+                uid = str(id(node))
+                opuid = uid + node.operator
+                graph.node(name=uid, label=f"data={node.data}", shape='record')
+                if node.operator:
+                    graph.node(name=opuid, label=f"{node.operator}")
+                    graph.edge(opuid, uid)
+                for operand in node.operands:
+                    open.add(operand)
+                    graph.edge(str(id(operand)), opuid)
+
+        return graph
 
 class Value:
 
-    def __init__(self, data, operator, operands):
+    def __init__(self, data, operator='', operands=()):
         self.data = data
         self.operator = operator
         self.operands = set(operands)
@@ -23,5 +46,16 @@ class Value:
     def __truediv__(self, other):
         return Value(self.data / other.data, '/', (self, other))
         
-    
+       
+
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.0)
+d = a * b + c
+print (d)
+graph = graph(d)
+graph.view()
+                    
+
+                    
 
