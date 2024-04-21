c564272199b0343e3836657b48d117f1412e9e36
GTAddict <soregtaddict@gmail.com>
2024-04-22 00:03:40 +0400

    Adding labels to graphviz

--- grad.py
+++ grad.py
@@ -14,7 +14,7 @@ def graph(value):
                 visited.add(node)
                 uid = str(id(node))
                 opuid = uid + node.operator
-                graph.node(name=uid, label=f"data={node.data}", shape='record')
+                graph.node(name=uid, label=f"{node.label} | data={node.data}", shape='record')
                 if node.operator:
                     graph.node(name=opuid, label=f"{node.operator}")
                     graph.edge(opuid, uid)
@@ -26,32 +26,34 @@ def graph(value):
 
 class Value:
 
-    def __init__(self, data, operator='', operands=()):
+    def __init__(self, data, operator='', operands=(), label='rvalue'):
         self.data = data
         self.operator = operator
         self.operands = set(operands)
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data}, operator={self.operator}, operands={self.operands})"
     
     def __add__(self, other):
-        return Value(self.data + other.data, '+', (self, other))
+        return Value(self.data + other.data, '+', (self, other), self.label + '+' + other.label)
     
     def __sub__(self, other):
-        return Value(self.data - other.data, '-', (self, other))
+        return Value(self.data - other.data, '-', (self, other), self.label + '-' + other.label)
     
     def __mul__(self, other):
-        return Value(self.data * other.data, '*', (self, other))
+        return Value(self.data * other.data, '*', (self, other), self.label + '*' + other.label)
     
     def __truediv__(self, other):
-        return Value(self.data / other.data, '/', (self, other))
+        return Value(self.data / other.data, '/', (self, other), self.label + '/' + other.label)
         
        
 
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a * b + c
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+d = (a * (b + c) + Value(10)) - Value(24)
+d.label = 'd'
 print (d)
 graph = graph(d)
 graph.view()
